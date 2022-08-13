a32738ca750886543f1b9637ffeef04f8961695e
tomascone <tomascone13@gmail.com>
2022-08-13 01:33:15 -0400

    Modelo, seeder, listar y ver productos, y navbar

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', [ProductController::class, 'index']);
+
+Route::controller(ProductController::class)
+    ->group(function () {
+        Route::get('/', 'index');
+        Route::get('/product/{product:id}', 'show');
+    });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
